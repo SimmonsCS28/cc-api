feea84cfffdcb8d75555f8b5cf2620aeed0fd960
Cole Simmons <SimmonsCS28@gmail.com>
2022-11-28 23:37:31 -0600

    BE Tournament Model
    Generated the back end object for tournaments

---
+++ Web/Models/TournamentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Web.Models
+{
+    public class TournamentModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxTeams { get; set; }   
+        public int FeeAmount { get; set; }
+        public int CurrentActiveTournmanet { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public int LastModifiedUserId { get; set; }
+
+    }
+}
